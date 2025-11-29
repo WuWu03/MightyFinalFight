b613dfadda7b256a27a880d85bcd3e63b53725cc
GQY <424215969@qq.com>
2025-11-29 18:21:26 +0800

    新版inputsystem

--- Assets/Scripts/Test/InputTest.cs
+++ Assets/Scripts/Test/InputTest.cs
@@ -1,59 +1,61 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.HID;
-using UnityEngine.Rendering;
 
 public class InputTest : MonoBehaviour
 {
-    private class InputEvent:Test.IPlayer1Actions
-    {
-        public void OnStick(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnButton(InputAction.CallbackContext context)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void OnKeyBoard(InputAction.CallbackContext context)
-        {
-            Debug.Log(context.ReadValue<float>());
-        }
-    }
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private InputAction action;
+    private TestInputMgr m_InputMgr = new();
+    public InputActionAsset asset;
     void Start()
     {
-        action = new InputAction("fuck");
-        InputBinding binding = new InputBinding("Keyboard/W");
-        action.AddBinding(binding);
-        action.Enable();
-        action.performed += OnInput;
+        m_InputMgr.inputActionAsset = asset;
+        m_InputMgr.SetCurrScheme("Game");
+        // m_InputMgr.AddInputEvent<Vector2>("LeftAxis",OnLeftAxis);
+        // m_InputMgr.AddInputEvent<float>("LT",OnLT);
+        // m_InputMgr.AddInputEvent("LB",OnLB);
+        // action = new InputAction("fuck");
+        // InputBinding binding = new InputBinding("Keyboard/W");
+        // action.AddBinding(binding);
+        // action.Enable();
+        // action.performed += OnInput;
     }
 
-    void Update()
+    private void OnLB()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("开始重新绑定");
-            action.Disable();
-            action.PerformInteractiveRebinding(0).OnComplete(operation =>
-            {
-                action.Enable();
-                operation.Dispose();
-                Debug.Log("重新绑定");
-            }).OnCancel(operation => {
-                action.Enable();
-                operation.Dispose();
-            }).Start();
-        }
+        Debug.Log("LB");
     }
-    private void OnInput(InputAction.CallbackContext obj)
+
+    private void OnLT(float t)
     {
-        string str = action.GetBindingDisplayString(0);
-        Debug.Log(str);
+        Debug.Log(t);
     }
+
+    private void OnLeftAxis(Vector2 t)
+    {
+        Debug.Log(t);
+    }
+
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.A))
+    //     {
+    //         Debug.Log("开始重新绑定");
+    //         action.Disable(); 
+    //         action.PerformInteractiveRebinding(0).OnComplete(operation =>
+    //         {
+    //             action.Enable();
+    //             operation.Dispose();
+    //             Debug.Log("重新绑定");
+    //         }).OnCancel(operation => {
+    //             action.Enable();
+    //             operation.Dispose();
+    //         }).Start();
+    //     }
+    // }
+    // private void OnInput(InputAction.CallbackContext obj)
+    // {
+    //     string str = action.GetBindingDisplayString(0);
+    //     Debug.Log(str);
+    // }
 }
